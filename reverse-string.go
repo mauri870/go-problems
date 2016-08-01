9a4e0c3588c99431a35a3aa586766e5124f35a32
Mauri de Souza Nunes <mauri870@gmail.com>
2016-08-01 16:24:13 -0300

    Reverse func now return only the reversed string

--- reverse-string.go
+++ reverse-string.go
@@ -9,11 +9,14 @@ func main() {
 	var s = flag.String("s", "Welcome to Go World!", "What you want to reverse?")
 	flag.Parse()
 
-	reverse(*s)
+	fmt.Println(reverse(*s))
 }
 
-func reverse(s string) {
+func reverse(s string) string {
+	reversed := ""
 	for i := len(s) - 1; i >= 0;i-- {
-		fmt.Printf("%s", string(s[i]))
+		reversed += string(s[i])
 	}
+
+	return reversed
 }
